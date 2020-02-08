fb4f509e085a28397b4aa9f2cacbdd2654057384
runo280 <runo280@users.noreply.github.com>
2020-02-08 11:18:35 +0330

    Update db.py

--- db.py
+++ db.py
@@ -7,8 +7,8 @@ db_url = 'mongodb+srv://{user}:{pwd}@{murl}/test?retryWrites=true&w=majority'
 db_user = os.environ['muser']
 db_pass = os.environ['mpass']
 db_domain = os.environ['murl']
-db_name = 'andev'
-db_feeds_collection = 'feeds'
+db_name = 'android'
+db_feeds_collection = 'andev'
 db_url = db_url.format(user=db_user, pwd=db_pass, murl=db_domain)
 client = pymongo.MongoClient(db_url)
 
